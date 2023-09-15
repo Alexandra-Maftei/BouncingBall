1e70e6482fd6d48f3c82a59ba1a19b66833791b8
Alexandra-Maftei <gmafteialexandra@gmail.com>
2023-09-15 15:09:10 +0300

    Add project files.

---
+++ Tema6/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Tema6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.animationPictureBox = new System.Windows.Forms.PictureBox();
+            this.changeColorButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.animationPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // animationPictureBox
+            // 
+            this.animationPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.animationPictureBox.Location = new System.Drawing.Point(103, 97);
+            this.animationPictureBox.Name = "animationPictureBox";
+            this.animationPictureBox.Size = new System.Drawing.Size(588, 466);
+            this.animationPictureBox.TabIndex = 0;
+            this.animationPictureBox.TabStop = false;
+            this.animationPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.animationPictureBox_Paint);
+            // 
+            // changeColorButton
+            // 
+            this.changeColorButton.Font = new System.Drawing.Font("Lucida Handwriting", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeColorButton.Location = new System.Drawing.Point(801, 150);
+            this.changeColorButton.Name = "changeColorButton";
+            this.changeColorButton.Size = new System.Drawing.Size(112, 48);
+            this.changeColorButton.TabIndex = 1;
+            this.changeColorButton.Text = "Culoare";
+            this.changeColorButton.UseVisualStyleBackColor = true;
+            this.changeColorButton.Click += new System.EventHandler(this.changeColorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 631);
+            this.Controls.Add(this.changeColorButton);
+            this.Controls.Add(this.animationPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.animationPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox animationPictureBox;
+        private System.Windows.Forms.Button changeColorButton;
+    }
+}
+
---
+++ Tema6/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema6
+{
+    public partial class Form1 : Form
+    {
+        private const int BoxWidth = 400;
+        private const int BoxHeight = 300;
+        private const int BallRadius = 20;
+        private const int BallSpeed = 5;
+
+        private Point ballPosition;
+        private Size ballVelocity;
+        private Color ballColor;
+
+        private Timer animationTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAnimation();
+        }
+        private void InitializeAnimation()
+        {
+            ballPosition = new Point(BoxWidth / 2, BoxHeight / 2);
+            ballVelocity = new Size(BallSpeed, BallSpeed);
+            ballColor = Color.Red;
+
+            animationTimer = new Timer();
+            animationTimer.Interval = 16; // Approximately 60 FPS
+            animationTimer.Tick += AnimationTimer_Tick;
+            animationTimer.Start();
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            // Actualizăm poziția mingii în funcție de viteză
+            ballPosition += ballVelocity;
+
+            // Verificăm coliziunile cu pereții cutiei
+            if (ballPosition.X <= 0 || ballPosition.X >= BoxWidth - BallRadius * 2)
+            {
+                ballVelocity.Width = -ballVelocity.Width; // Inversăm direcția pe axa X
+            }
+
+            if (ballPosition.Y <= 0 || ballPosition.Y >= BoxHeight - BallRadius * 2)
+            {
+                ballVelocity.Height = -ballVelocity.Height; // Inversăm direcția pe axa Y
+            }
+
+            // Redesenăm cutia și mingea
+            animationPictureBox.Invalidate();
+        }
+        private void animationPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            // Desenăm cutia
+            e.Graphics.DrawRectangle(Pens.Black, 0, 0, BoxWidth - 1, BoxHeight - 1);
+
+            // Desenăm mingea
+            using (SolidBrush brush = new SolidBrush(ballColor))
+            {
+                e.Graphics.FillEllipse(brush, ballPosition.X, ballPosition.Y, BallRadius * 2, BallRadius * 2);
+            }
+        }
+        private void changeColorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                ballColor = colorDialog.Color;
+            }
+        }
+    }
+}
